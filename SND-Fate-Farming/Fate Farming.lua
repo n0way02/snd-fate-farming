4e44aa99d68602584dc6dbb64046236fb4784da3
Lima <159471546+n0way02@users.noreply.github.com>
2026-04-04 10:38:37 -0300

    Update version to 3.1.5 and enhance Fate Tracker
    Updated version to 3.1.5 and fixed inventory issue for Bango Zango. Enhanced Fate Tracker integration with additional support for Map, Server, and Gemstone Tracker.

--- SND-Fate-Farming/Fate Farming.lua
+++ SND-Fate-Farming/Fate Farming.lua
@@ -1,7 +1,7 @@
 --[=====[
 [[SND Metadata]]
 author: n0way (fork from pot0to script)
-version: 3.1.4
+version: 3.1.5
 description: >-
   Fate farming script with the following features:
 
@@ -153,6 +153,9 @@ configs:
 *                                  Changelog                                   *
 ********************************************************************************
 
+    -> 3.1.5    by: n0way02 (https://ko-fi.com/n0way02)
+                Fix "Insufficient inventory space" for Bango Zango when buying Gysahl Greens (Updated Item Index).
+                Enhanced Fate Tracker integration: added Map, Server and Gemstone Tracker support to real-time UI.
     -> 3.1.4    by: n0way02 (https://ko-fi.com/n0way02)    
                 Added real-time FATE tracking integration for web dashboard.
     -> 3.1.3    by: n0way02 (https://ko-fi.com/n0way02)    
